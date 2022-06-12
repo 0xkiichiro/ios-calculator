67cb2ddf4cc9683d3f60aaa156fd918a10544ba2
0xkiichiro <barisaytimur@gmail.com>
2022-06-12 21:50:13 +0300

    js start

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+//! variable declarions
+
+const currResult = document.querySelector(".current-result");
+const prevResult = document.querySelector(".previous-result");
+
+//! click capture
+
 document
   .querySelector(".calculator-body")
   .addEventListener("click", (event) => {
-    console.log(event.target);
+    if (event.target.classList.contains("num")) {
+      render(event.target);
+    } else if (event.target.classList.contains("operator")) {
+      selectOperation(event.target);
+    }
   });
+
+//! functions
+
+const calculate = () => {};
+
+const render = (num) => {
+  currResult.innerText += num.innerText;
+};
+
+const selectOperation = (operator) => {};
